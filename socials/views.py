cf9a6bf4ff6cdf51e6a8d6a11aefbf943cdd598b
Ansah-98 <yungtupac99@gmail.com>
2022-06-11 23:17:56 +0000

    search and deletion of rooms

--- socials/views.py
+++ socials/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render,redirect
-from .models import Room
+from .models import Room,Topic
 from .forms import RoomForm
+from django.db.models import  Q
 # Create your views here.
 def home(request):
-    rooms = Room.objects.all()
-    context={'rooms':rooms}
+    
+    
+    q = request.GET.get('q')
+    if q is None:
+        topic = Topic.objects.all()
+        rooms = Room.objects.all()
+    else:
+        topic = Topic.objects.filter(title__icontains = q)
+        
+        rooms = Room.objects.filter(Q(topic__title__icontains =q)|
+        Q(description__icontains =q)|
+        Q(name__icontains=q)|
+        Q(host__username__icontains=q))
+    
+
+    room_count = rooms.count()
+    context={'rooms':rooms, 'topics': topic, 'room_count':room_count}
     return render(request,'socials/index.html',context)
+
 def room(request,pk):
 
     room = Room.objects.get(id = pk)
@@ -28,9 +45,17 @@ def updateRoom(request,pk):
     form = RoomForm(instance =room)
     context = {'form':form}
     if request.method == 'POST':
-        form = RoomForm(request.POST, instance= room)
+        form = RoomForm(request.POST, instance = room)
         if form.is_valid():
             form.save()
             return redirect('home')
     
     return render(request,'socials/room_form.html', context)
+
+def deleteRoom(request,pk):
+    room = Room.objects.get(pk=pk)
+    if request.method =='P0ST':
+        room.delete()
+        return redirect('home')
+    
+    return render(request, 'socials/delete.html',{'obj':room.name})
